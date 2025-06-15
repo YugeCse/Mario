b2a4e63c187ea674f654afd5432b4e690e379865
Hwang <yugecosecant@foxmail.com>
2025-06-15 20:58:28 +0800

    增加游戏主图层，增加对关卡标识的碰撞逻辑

--- global/GlobalConfig.gd
+++ global/GlobalConfig.gd
@@ -1,12 +1,20 @@
-extends Node
 ## 全局配置信息
-class_name GlobalConfig
+extends Node
 
-## 地图宽度
+## 地图宽度-640
 const MAP_WIDTH = 640
 
-## 地图高度
+## 地图高度-480
 const MAP_HEIGHT = 480
 
 ## 地图尺寸 W:H
 const MAP_SIZE = Vector2(MAP_WIDTH, MAP_HEIGHT)
+
+## 地图地块大小-16
+const MAP_TILE_SIZE = 16
+
+## 当前游戏等级-1
+var current_level: int = 1
+
+## 玩家生命数
+var player_life_count: int = 3
