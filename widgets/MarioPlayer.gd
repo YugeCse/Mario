83efcd8428005d9836a0bcc1ba983285a9c3f65f
Hwang <huangzhongyu@sph.com.sg>
2025-06-20 10:32:50 +0800

    #增加物品层，如：金币、金砖，并摆放他们的位置

--- widgets/MarioPlayer.gd
+++ widgets/MarioPlayer.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-## 上跳的垂直方向值
+## 上跳的垂直方向值：-360.0
 const JUMP_FORCE = -360.0
 
-## 单次最大可跳跃次数
+## 单次最大可跳跃次数：2
 const JUMP_MAX_COUNT = 2
 
-## 地心引力值：980.0
+## 重力值：980.0
 var gravity: float = 980.0
 
-## 行走时的速度
+## 行走时的速度：120为初始速度
 var walk_speed: float = 120.0
 
 ## 跳跃计数，最大不超过2次
@@ -34,21 +34,31 @@ func _physics_process(delta: float) -> void:
 			velocity.y = JUMP_FORCE
 		elif jump_count < JUMP_MAX_COUNT:
 			jump_count += 1
-			velocity.y = JUMP_FORCE * 0.9
+			velocity.y = JUMP_FORCE * 0.9 # 二次跳，垂直向上有衰减
 	was_on_floor = is_on_floor_now
 	var target_velocity_x = 0.0
 	if Input.is_action_pressed(&'ui_left'):
 		$ASprites.play('walk')
 		$ASprites.flip_h = true
-		target_velocity_x = - walk_speed
+		target_velocity_x = - walk_speed # 左移时速度为负
 	elif Input.is_action_pressed(&'ui_right'):
 		$ASprites.play('walk')
 		$ASprites.flip_h = false
-		target_velocity_x = walk_speed
+		target_velocity_x = walk_speed # 右移时速度为正
 	else:
 		$ASprites.play('default')
-	if not is_on_floor_now:
-		velocity.x = lerp(velocity.x, target_velocity_x, 0.8)
+		target_velocity_x = 0.0 # 没有左右按键时，水平速度为0
+	if not is_on_floor_now: # 如果不在地上，水平速度变化慢一些
+		velocity.x = lerp(velocity.x, target_velocity_x, 0.8) # 在空中时，水平速度变化慢一些
 	else:
-		velocity.x = target_velocity_x
-	move_and_slide() # 移动并执行滑动
+		velocity.x = target_velocity_x # 在地上时，水平速度是正常的
+	if not move_and_slide(): # 移动并执行滑动
+		return # 如果移动未发生碰撞，则不执行下面的代码
+
+## 处理碰撞事件
+func _handle_collision_events() -> void:
+	var collider = get_last_slide_collision() # 获取最后一次滑动碰撞
+	print_debug('发生了碰撞事件，被碰撞者是：' + collider.name)
+	# if collider.is_in_group('enemy'):
+	# 	velocity.y = JUMP_FORCE # 跳起来
+	# 	velocity.x = 0.0 # 停止水平移动
